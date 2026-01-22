7ee274530f025f8f54b5498ae027c2dfb79d49fc
raj-mavani6 <rajmavani19@gmail.com>
2026-01-22 19:07:27 +0530

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NotesSystemAdmin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
